0ed5aeb301ade3581119f5ab492d134fd45f2e50
enes1094 <enes.tikici123@hotmail.com>
2023-07-14 01:29:52 +0300

    added repository

--- SehirRehperi.API/Data/IAppRepository.cs
+++ SehirRehperi.API/Data/IAppRepository.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using SehirRehperi.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SehirRehperi.API.Data
 {
-    interface IAppRepository
+    public interface IAppRepository
     {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        bool SaveAll();
+
+        List<City> GetCities();
+        List<Photo> GetPhotosByCity(int cityId);
+        City GetCityById(int cityId);
+
+        Photo GetPhoto(int id);
     }
 }
