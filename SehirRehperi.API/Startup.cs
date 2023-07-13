919da0b738de0a573c3c3d0c268ee18256b3755f
enes1094 <enes.tikici123@hotmail.com>
2023-07-14 01:04:53 +0300

    added new models

--- SehirRehperi.API/Startup.cs
+++ SehirRehperi.API/Startup.cs
@@ -30,6 +30,7 @@ namespace SehirRehperi.API
             services.AddDbContext<DataContext>(x =>
                 x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +44,8 @@ namespace SehirRehperi.API
             {
                 app.UseHsts();
             }
+
+            app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
 
             app.UseHttpsRedirection();
             app.UseMvc();
